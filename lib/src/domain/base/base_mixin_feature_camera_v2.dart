7d4dd0fad78193cff7ebe1b1835dcb74b57756e7
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-09-16 23:16:42 +0700

    feature(streamCamera): revert mixin

--- lib/src/domain/base/base_mixin_feature_camera_v2.dart
+++ lib/src/domain/base/base_mixin_feature_camera_v2.dart
@@ -15,7 +15,7 @@ import 'package:image/image.dart' as image_lib;
 ///
 /// This mixin can be used in widgets or classes where camera functionality is required, allowing for
 /// streamlined integration of camera operations in Flutter applications.
-class BaseMixinFeatureCameraV2 {
+mixin BaseMixinFeatureCameraV2 {
   /// The current [CameraController] to control the camera hardware.
   CameraController? cameraController;
 
