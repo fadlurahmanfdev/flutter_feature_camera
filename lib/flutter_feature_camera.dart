7ccff354c178ead40580acc8c0b64a5f934acfe4
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-11-08 13:14:51 +0700

    feature(library): remove deprecated

--- lib/flutter_feature_camera.dart
+++ lib/flutter_feature_camera.dart
@@ -6,9 +6,5 @@ export 'src/data/model/capture_image_model.dart';
 
 export 'src/domain/base/base_mixin_feature_camera_v2.dart';
 
-export 'src/presentation/custom_clipper/circle_clipper.dart';
-export 'src/presentation/custom_clipper/rectangle_clipper.dart';
-export 'src/presentation/custom_painter/circle_painter.dart';
 export 'src/presentation/custom_painter/circle_painter_v2.dart';
-export 'src/presentation/custom_painter/rectangle_painter.dart';
 export 'src/presentation/custom_painter/rectangle_painter_v2.dart';
